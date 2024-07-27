3e6288ce21d78f047dfccba59a63f9d4af7b12b3
Cellad1421 <jgomezc943@ulacit.ed.cr>
2024-07-26 19:35:05 -0600

    Actualizacion BD 26-07-2024

--- Scripts_BD/Drop_Table.sql
+++ Scripts_BD/Drop_Table.sql
@@ -1,6 +1,14 @@
 Use TicoGourmet
 go
 
+--DROP TRIGGER
+
+DROP TRIGGER trg_CheckRolOnInsert_Empleados;
+DROP TRIGGER trg_CheckRolOnInsert_Meseros;
+DROP TRIGGER trg_CheckRolOnInsert_Reservas;
+
+--DROP TABLE
+
 DROP TABLE Meseros
 DROP TABLE Empleados
 DROP TABLE Reservas
